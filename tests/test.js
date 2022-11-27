536db25d9e2661a971acf88305b88a9076b1d549
Iván Pérez <lukegothic@gmail.com>
2022-11-27 12:47:25 +0100

    final README.MD

---
+++ tests/test.js
@@ -0,0 +1,11 @@
+describe("User", function () {
+  describe("#save()", function () {
+    it("should save without error", function (done) {
+      var user = new User("Luna");
+      user.save(function (err) {
+        if (err) done(err);
+        else done();
+      });
+    });
+  });
+});
